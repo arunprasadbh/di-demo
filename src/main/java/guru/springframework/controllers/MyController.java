801a2275a05e7d1dbd71bc1235c410d80759060b
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-02 06:58:03 +0100

    Corrected packages. This is first working copy

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
     public String hello(){
         System.out.println("Hello !!");
         return "foo";
